a1b5fee516f2e6261b48dd5b0e3e1998e590e775
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2012-02-27 23:19:53 +0100

    Fixed DevelopmentToolbar public definition name

---
+++ Scripts/App/Plugins/DevelopmentToolbar.js
@@ -0,0 +1,30 @@
+/*
+ * DevelopmentToolbar.js
+ * 
+ * Author:
+ * 		Aleksandar Toplek,
+ *
+ * Created on:
+ * 		27.02.2012.
+ *
+ */
+
+// Here goese public function definitions
+ var DevelopmentToolbar = {
+ 	"Register": 		Register
+ }
+
+
+/*
+ * Registers Developer toolbar plugin
+ */
+ function Register() {
+ 	Helpers.Log("Registering DevelopmentToolbar plugin...");
+
+ 	if (IsDevelopmentMode == false) {
+ 		Helpers.Log("Plugin set to inactive due to extension not in development mode.");
+ 	}
+ 	else {
+ 		// TODO: Implement registration here
+ 	}
+ }
