7d257592ce832d53a4c32ca3d096d6bd30a7c909
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2012-03-03 21:46:15 +0100

    DevelopmentToolbar changes

---
+++ Scripts/App/NotificationManager.js
@@ -0,0 +1,39 @@
+/*
+ * Notifications.js
+ *
+ * Author:
+ *		Aleksandar Toplek
+ *
+ * Created on:
+ *		25.02.2012.
+ *
+ */
+
+// Definitions
+var NotificationManager = {
+	"Show": 				Show,
+	"Notification": 		Notification
+};
+
+// TODO: Comment object
+function Notification(image, header, message, timeout) {
+ 	this.image = image,
+ 	this.header = header,
+ 	this.message = message,
+ 	this.timeout = timeout
+ };
+
+ // TODO: Comment function
+function Show(notification) {
+	// Creates new notification object
+	var notification = webkitNotifications.createNotification(
+		notification.image, 
+		notification.header, 
+		notification.message);
+
+	// Shows notification window
+	notification.show();
+
+	// Sets notification timeout to given value or default (5000 ms)
+	setTimeout(function() { notification.cancel(); }, notification.timeout || 5000);
+};
---
+++ Scripts/App/RequestManager.js
@@ -0,0 +1,52 @@
+/*
+ * Requests.js
+ * 
+ * Author:
+ * 		Aleksandar Toplek,
+ *
+ * Created on:
+ * 		25.02.2012.
+ *
+ */
+
+ // Definition
+ var RequestManager = {
+ 	"Request": 			Request,
+ 	"Send": 			Send,
+ 	"Recieve": 			Recieve
+ };
+
+
+
+
+// TODO: Comment class
+function Request(requestSign, requestCategory, requestName, actionName, requestData) {
+	this.requestSign = requestSign,
+	this.requestCategory = requestCategory,
+	this.requestName = requestName,
+	this.actionName = actionName,
+	this.requestData = requestData
+};
+
+
+// TODO: Comment function
+function Recieve(sign, callback) {
+	chrome.extension.onRequest.addListener(
+		function(request, sender, sendResponse) {
+			if (sign == request.requestSign) {
+				callback(request, sender, sendResponse);
+			}
+		}
+	);
+};
+
+/*
+ * Sends request with data and callback to 
+ * listener. 
+ *
+ * callback can be {Null} - in that case it is
+ *          replaces with empty funtion call    
+ */
+ function Send(request, callback) {
+ 	chrome.extension.sendRequest(request, callback || function() {});
+ };
