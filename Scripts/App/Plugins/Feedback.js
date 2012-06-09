ccd6812af430176ceb860e060967fe071b42cc8b
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2012-06-09 23:38:06 +0200

    Feedback now fully functional

--- Scripts/App/Plugins/Feedback.js
+++ Scripts/App/Plugins/Feedback.js
@@ -40,7 +40,23 @@ function Feedback() {
 	 * Registers Feedback plugin
 	 *
 	 *************************************************************************/
-	this.Register = function() {
-		
-	}
+	this.Register = function () {
+		// Insert feedback image
+		$("#logo").after("<div style='margin:24px 31px 0 0;width:44px;height:33px;float:left;'><img id='PAFeedback' src='" + Helpers.GetImageURL("Plugins", "Feedback.png") + "' width='43px' height='43px' style='-webkit-filter:grayscale(1);' /></div>");
+
+		// Remove spacer
+		// TODO check if this  spacer means anything cause it has its own id maybe only for styling
+		$("#myGameLinkHeaderWrapper").remove();
+
+		// On mouse over/leave grayscale effect
+		$("#PAFeedback").mouseenter(function () { $("#PAFeedback").attr("style", ""); }).mouseleave(function () { $("#PAFeedback").attr("style", "-webkit-filter:grayscale(1);"); });
+
+		// Show popup on feedback image click
+		$("#PAFeedback").click(function () {
+			var popupWindow = window.open(Helpers.GetExtensionRootURL("Scripts/App/Plugins/FeedbackForm.html"), "name", "height=400, width=300");
+			if(window.focus) {
+				popupWindow.focus();
+			}
+		});
+	};
 }
