b8510a78dc62a3d3ac872e7de5b365d4828740a0
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2014-07-19 01:49:07 +0200

    BackgroundScript removed localStorage related code

--- Pages/Background/BackgroundScript.js
+++ Pages/Background/BackgroundScript.js
@@ -17,7 +17,7 @@ backgroundScript.Initialize();
 /// </summary>
 function BackgroundScript() {
 	var Settings = {
-		TravianURLMatchesKey: "PATravianServerMatches"
+
 	};
 
 	var Default = {
@@ -34,12 +34,6 @@ function BackgroundScript() {
 	this.Initialize = function () {
 		// Initial setup
 		InitialSetup();
-
-		// Check if localStorage is supported
-		if (typeof (localStorage) == 'undefined') {
-			isLocalStorageSupported = false;
-			console.error("localStorage not found! Try updating your browser!");
-		}
 
 		// Attach listener to all request signs
 		requestManager.Recieve("*", GotRequest);
@@ -80,13 +74,8 @@ function BackgroundScript() {
 			chrome.tabs.sendMessage(sender.tab.id, request);
 		} else {
 			// Supports following categories
-			//		Data
 			//		Action
 			switch (request.Category) {
-				case "Data": {
-					GotDataRequest(request, sendResponse);
-					break;
-				}
 				case "Action": {
 					GotActionRequest(request);
 					break;
@@ -116,88 +105,12 @@ function BackgroundScript() {
 		else ActionsAvailable[request.Name]();
 	};
 
-	var GotDataRequest = function (request, sendResponse) {
-		/// <summary>
-		/// Handles data requests
-		/// </summary>
-		/// <param name="request">Request object</param>
-		/// <param name="sendResponse">Response function</param>
-
-		console.log("Got Data request [" + request.Data.Type + "]");
-
-		if (request.Data.Type == "get") {
-			sendResponse(GetObject(request.Name));
-		}
-		else if (request.Data.Type == "set") {
-			SetObject(request.Name, request.Data.Value);
-		}
-		else {
-			console.error("Unknown Data request Type [" + request.Data.Type + "]");
-			console.error(request);
-		}
-	};
-
-	var ActionIsFirstPlay = function () {
-		/// <summary>
-		/// Handles IsFirstPlay action request
-		/// </summary>
-		
-		if (!GetObject("IsFirstPlay")) {
-			chrome.tabs.create({ url: GetURL("Pages/Welcome/Welcome.html") });
-			SetObject("IsFirstPlay", { State: "AlreadyPlayed" });
-		};
-	};
-
-	var ActionReloadExtension = function() {
-		chrome.tabs.create({ url: "http://reload-extension/", active: false });
-	};
-
 	var ActionsAvailable = {
 		/// <summary>
 		/// List of actions that can be called
 		/// </summary>
-		
-		IsFirstPlay: ActionIsFirstPlay,
-		ReloadExtension: ActionReloadExtension
-	};
-	
-	var SetObject = function (key, value) {
-		/// <summary>
-		/// Sets object to localStorage as <key, value> pair
-		/// This function will automatically stringify given object.
-		/// </summary>
-		/// <param name="key">Key object</param>
-		/// <param name="value">Value object</param>
-		
-		try {
-			localStorage.setItem(key, JSON.stringify(value));
-			console.log("Set Data [" + key + "] Value");
-			console.log(value);
-		}
-		catch (exception) {
-			if (exception.name === 'QUOTA_EXCEEDED_ERR' ||
-				exception.name === 'NS_ERROR_DOM_QUOTA_REACHED') {
-				console.error("Quota exceeded! Clear localStorage to solve problem. WARNING: Clearing localStorage will delete all user data.");
-			}
-			else {
-				console.error("Unknown error while trying to set an item \"" + key + "\" with value: ");
-				console.log(value);
-			}
-		}
-	};
-
-	/// <summary>
-	/// Gets object from localStorage as value for given key
-	/// This function will automatically parse localStorage value and return object type
-	/// </summary>
-	/// <param name="key">Key object</param>
-	/// <returns>Object from localStorage that corresponds to given key</returns>
-	var GetObject = function (key) {
-		var value = localStorage.getItem(key);
-		var parsedValue = value && JSON.parse(value);
-		console.log("Get Data [" + key + "] Value");
-		console.log(parsedValue);
-		return parsedValue;
+		// Example: 
+		// ActionName: ActionFunction
 	};
 };
 
