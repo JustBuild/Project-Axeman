e330e0bd518aede1fd3c8ab5d6ba5b191efee99f
Matt <info@mgates.me>
2015-03-03 15:43:45 -0500

    Fix syntax error

--- Plugins/Fun/Decorations/Decorations.js
+++ Plugins/Fun/Decorations/Decorations.js
@@ -39,19 +39,19 @@ function Decorations() {
 	};
 
 	var RemovePlusAnimation = function() {
-		$('head').append('<style id="addedCSS" type="text/css">
-			ul#navigation li.gold a.ani_1{background-position:-1 !important;}
-			ul#navigation li.gold a.ani_2{background-position:-1 !important;}
-			ul#navigation li.gold a.ani_3{background-position:-1 !important1}
-			ul#navigation li.gold a.ani_4{background-position:-1 !important;}
-			ul#navigation li.gold a.ani_5{background-position:-1 !important;}
-			ul#navigation li.gold a.ani_6{background-position:-1 !important;}
-			ul#navigation li.gold a.ani_7{background-position:-1 !important;}
-			ul#navigation li.gold a.ani_8{background-position:-1 !important;}
-			ul#navigation li.gold a.ani_9{background-position:-1 !important;}
-			ul#navigation li.gold a.ani_10{background-position:-1 !important;}
-			ul#navigation li.gold a.ani_11{background-position:-1 !important;}
-			ul#navigation li.gold a.ani_12{background-position:-1 !important;}
+		$('head').append('<style id="addedCSS" type="text/css">\
+			ul#navigation li.gold a.ani_1{background-position:-1 !important;}\
+			ul#navigation li.gold a.ani_2{background-position:-1 !important;}\
+			ul#navigation li.gold a.ani_3{background-position:-1 !important1}\
+			ul#navigation li.gold a.ani_4{background-position:-1 !important;}\
+			ul#navigation li.gold a.ani_5{background-position:-1 !important;}\
+			ul#navigation li.gold a.ani_6{background-position:-1 !important;}\
+			ul#navigation li.gold a.ani_7{background-position:-1 !important;}\
+			ul#navigation li.gold a.ani_8{background-position:-1 !important;}\
+			ul#navigation li.gold a.ani_9{background-position:-1 !important;}\
+			ul#navigation li.gold a.ani_10{background-position:-1 !important;}\
+			ul#navigation li.gold a.ani_11{background-position:-1 !important;}\
+			ul#navigation li.gold a.ani_12{background-position:-1 !important;}\
 		</style>');
 	};
 
